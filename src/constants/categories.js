de711676a0f09877dc6ecc2ee18635991cbde301
vivek2034 <gamervivek3004@gmail.com>
2026-08-03 22:51:05 +0530

    Update categories.js

--- src/constants/categories.js
+++ src/constants/categories.js
@@ -1,7 +1,12 @@
 export const STORY_CATEGORIES = [
   "Dark",
+  "Comedy",
+  "Crime",
   "Horror",
   "Fantasy",
+  "Mythology",
+  "Magical",
+  "Mystery",
   "Romance",
   "Dark Romance",
   "Lusty"
